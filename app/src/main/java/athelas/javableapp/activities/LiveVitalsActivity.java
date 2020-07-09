657bd2c4e5e1349d621773ffc0be4372d6abf237
unbun <uwh.547@gmail.com>
2020-07-09 05:12:36 -0400

    graph clean up

--- app/src/main/java/athelas/javableapp/activities/LiveVitalsActivity.java
+++ app/src/main/java/athelas/javableapp/activities/LiveVitalsActivity.java
@@ -12,7 +12,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,13 +24,15 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import athelas.javableapp.utils.BluetoothConnectionService;
 import athelas.javableapp.R;
 import athelas.javableapp.utils.Utils;
 import athelas.javableapp.utils.XYValue;
 
-public class LiveVitalsActivity extends AppCompatActivity {
+public class LiveVitalsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     public static String TAG = "LiveVitalsActivity";
 
     TextView tvIncomingMessages;
@@ -36,11 +41,20 @@ public class LiveVitalsActivity extends AppCompatActivity {
     BluetoothConnectionService mBTConnection;
 
     private LineGraphSeries<DataPoint> vitalSeries;
-    private ArrayList<XYValue> xyValueList;
+    private ArrayList<XYValue> currXYValues;
+    private ArrayList<XYValue> heartValues, bloodO2Values, tempValues, lungValues;
     GraphView mLinePlot;
+
+    Spinner testSelect;
+    int currTestColor;
+    Map<String, Integer> testToColor;
+    ArrayAdapter<CharSequence> testAdapter;
 
     double currX, currY;
     double startTimeMs = 0;
+
+    final int xAxisSize = 20;
+
     private BroadcastReceiver mReadReciever = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -62,7 +76,7 @@ public class LiveVitalsActivity extends AppCompatActivity {
                     currY = Double.parseDouble(readMessages.toString().replaceAll("\\D+", ""));
                     currX = (System.currentTimeMillis() - startTimeMs) / 1000.0f;
 
-                    xyValueList.add(new XYValue(currX, currY));
+                    currXYValues.add(new XYValue(currX, currY));
                     Log.d(TAG, "mReadReciever: plotting data to graph (" + currY + ", " + currX + ")");
 
                     initGraph();
@@ -85,18 +99,36 @@ public class LiveVitalsActivity extends AppCompatActivity {
         mBTConnection = MainActivity.getBluetoothConnection();
 
         readMessages = new StringBuilder();
-
         tvIncomingMessages = (TextView) findViewById(R.id.incomingMessages);
 
         //Use local broadcast manager to recieve incoming messages
         LocalBroadcastManager.getInstance(this).registerReceiver(mReadReciever, new IntentFilter("incomingMessage"));
 
+        testSelect = (Spinner) findViewById(R.id.testSelection);
+        testAdapter =
+                ArrayAdapter.createFromResource(this, R.array.tests, android.R.layout.simple_spinner_item);
+        testAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        testSelect.setAdapter(testAdapter);
+
+        testToColor = new HashMap<>();
+        testToColor.put("heart rate", Color.argb(255,233,30,90));
+        testToColor.put("blood o2", Color.argb(255, 3, 169, 244));
+        testToColor.put("temperature", Color.argb(255, 255, 87, 34));
+        testToColor.put("lung audio", Color.argb(255, 104, 89, 84));
+        currTestColor = Color.argb(100,233,30,90);
+
         mLinePlot = (GraphView) findViewById(R.id.vitalGraph);
-        xyValueList = new ArrayList<>();
+        heartValues = new ArrayList<>();
+        bloodO2Values = new ArrayList<>();
+        tempValues = new ArrayList<>();
+        lungValues = new ArrayList<>();
+
+        currXYValues = heartValues;
+
+        testSelect.setOnItemSelectedListener(LiveVitalsActivity.this);
 
         startTimeMs = System.currentTimeMillis();
         currX = 0;
-
         initGraph();
 
     }
@@ -104,7 +136,7 @@ public class LiveVitalsActivity extends AppCompatActivity {
     private void initGraph(){
         vitalSeries = new LineGraphSeries<>();
 
-        if(xyValueList.size() != 0) {
+        if(currXYValues.size() != 0) {
             createScatterPlot();
         } else {
             Log.d(TAG, "onCreate: No data to plot");
@@ -114,13 +146,13 @@ public class LiveVitalsActivity extends AppCompatActivity {
     private void createScatterPlot() {
         Log.d(TAG, "createScatterPlog: Creating scatter plot.");
         
-        xyValueList = Utils.sortArrayByX(xyValueList);
+        heartValues = Utils.sortArrayByX(currXYValues);
 
         double maxX = 0;
-        for(int ii = 0; ii < xyValueList.size(); ii++) {
+        for(int ii = 0; ii < currXYValues.size(); ii++) {
             try {
-                double x = xyValueList.get(ii).getX();
-                double y = xyValueList.get(ii).getY();
+                double x = currXYValues.get(ii).getX();
+                double y = currXYValues.get(ii).getY();
                 vitalSeries.appendData(new DataPoint(x,y), true, 1000);
                 maxX = Math.max(x, maxX);
             } catch (IllegalArgumentException e) {
@@ -131,7 +163,7 @@ public class LiveVitalsActivity extends AppCompatActivity {
         //set some properties
         vitalSeries.setDrawDataPoints(true);
         vitalSeries.setDataPointsRadius(5f);
-        vitalSeries.setColor(Color.BLUE);
+        vitalSeries.setColor(currTestColor);
 
         //set Scrollable and Scaleable
         mLinePlot.getViewport().setScalable(true);
@@ -147,7 +179,7 @@ public class LiveVitalsActivity extends AppCompatActivity {
         //set manual y bounds
         mLinePlot.getViewport().setXAxisBoundsManual(true);
         mLinePlot.getViewport().setMaxX(maxX + 1);
-        mLinePlot.getViewport().setMinX(Math.max(maxX - 30, 0));
+        mLinePlot.getViewport().setMinX(Math.max(maxX - xAxisSize, 0));
 
         mLinePlot.addSeries(vitalSeries);
 
@@ -192,5 +224,29 @@ public class LiveVitalsActivity extends AppCompatActivity {
         } else if(resultCode == RESULT_CANCELED) {
             Toast.makeText(getApplicationContext(), "Robot Controlling Failed", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d(TAG, "onItemSelected: changing currentTest");
+        String target = testAdapter.getItem(i).toString().toLowerCase();
+        currTestColor = testToColor.get(target);
+        tvIncomingMessages.setBackgroundColor(currTestColor);
+        switch (target) {
+            case "heart rate": currXYValues = heartValues;
+            break;
+            case "blood o2": currXYValues = bloodO2Values;
+            break;
+            case "temperature": currXYValues = tempValues;
+            break;
+            case "lung audio": currXYValues = lungValues;
+            break;
+        }
+        initGraph();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
